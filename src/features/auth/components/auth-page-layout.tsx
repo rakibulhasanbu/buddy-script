4dae7a99ff99e9365de84d9808d67d379d5ec918
rakibulhasanbu <rakibulhasanbu@gmail.com>
2026-07-12 18:14:25 +0600

    style(auth): refine layout and improve responsiveness of AuthPageLayout component

--- src/features/auth/components/auth-page-layout.tsx
+++ src/features/auth/components/auth-page-layout.tsx
@@ -9,20 +9,20 @@ interface AuthPageLayoutProps {
 
 export const AuthPageLayout = ({ children, illustration }: AuthPageLayoutProps) => {
   return (
-    <section className="relative z-[1] min-h-screen w-full overflow-hidden bg-[#F0F2F5] py-[100px]">
-      <div className="pointer-events-none absolute top-0 left-0 z-[-1]">
+    <section className="relative z-1 min-h-screen w-full overflow-hidden bg-[#F0F2F5] py-25">
+      <div className="pointer-events-none absolute top-0 left-0 z-[-1] hidden lg:block">
         <Image src="/images/shape1.svg" alt="" width={150} height={150} unoptimized className="block" />
       </div>
-      <div className="pointer-events-none absolute top-0 right-5 z-[-1]">
+      <div className="pointer-events-none absolute top-0 right-5 z-[-1] hidden lg:block">
         <Image src="/images/shape2.svg" alt="" width={450} height={450} unoptimized className="block" />
       </div>
-      <div className="pointer-events-none absolute right-[327px] bottom-0 z-[-1] hidden xl:block">
+      <div className="pointer-events-none absolute right-81.75 bottom-0 z-[-1] hidden xl:block">
         <Image src="/images/shape3.svg" alt="" width={494} height={494} unoptimized className="block" />
       </div>
 
       <div className="container mx-auto px-4 sm:px-6 lg:px-8">
         <div className="flex w-full items-center">
-          <div className="hidden w-full max-w-[66.666%] flex-shrink-0 items-center justify-center pr-8 lg:flex xl:pr-12">
+          <div className="hidden w-full max-w-[66.666%] shrink-0 items-center justify-center pr-8 lg:flex xl:pr-12">
             {illustration}
           </div>
           <div className="w-full lg:max-w-[33.333%]">{children}</div>
